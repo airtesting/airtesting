fe280e431f0644052cfcea932a47c4c86f7c5690
AnaCecilia65 <acsouza1415@gmail.com>
2016-07-31 18:19:25 -0300

    remove the comment

--- Jabuti/src/br/jabuti/cmdtool/CoverageSummary.java
+++ Jabuti/src/br/jabuti/cmdtool/CoverageSummary.java
@@ -15,7 +15,6 @@
     You should have received a copy of the GNU Lesser General Public License
     along with Jabuti.  If not, see <http://www.gnu.org/licenses/>.
 */
-//just a test
 
 package br.jabuti.cmdtool;
 
